80d32c13095fb606acd2dadadde74f438fb42a0c
ishukhan786 <ishukhan786@github.com>
2026-07-30 14:33:45 +0500

    style: reduce sidebar width from 270px to 240px

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -15,7 +15,7 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-const SIDEBAR_WIDTH = "270px";
+const SIDEBAR_WIDTH = "240px";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
 const SIDEBAR_WIDTH_ICON = "78px";
 const SIDEBAR_KEYBOARD_SHORTCUT = "b";
