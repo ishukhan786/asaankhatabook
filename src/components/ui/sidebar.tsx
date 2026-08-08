c5dc3a9cc076cee7d70fbde6ca9fa5bf58a9cfd3
ishukhan786 <ishukhan786@github.com>
2026-08-08 17:36:28 +0500

    Change collapsed sidebar width (SIDEBAR_WIDTH_ICON) to 40px

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -17,7 +17,7 @@ const SIDEBAR_COOKIE_NAME = "sidebar:state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
 const SIDEBAR_WIDTH = "240px";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
-const SIDEBAR_WIDTH_ICON = "78px";
+const SIDEBAR_WIDTH_ICON = "40px";
 const SIDEBAR_KEYBOARD_SHORTCUT = "b";
 
 type SidebarContext = {
